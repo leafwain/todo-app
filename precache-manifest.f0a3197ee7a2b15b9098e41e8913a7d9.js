6f54c1e6ad4c8db221c46aa49f50e66b6181fde0
leafwain <lutsky_nik@mail.ru>
2023-06-06 11:58:11 +0700

    Updates

---
+++ precache-manifest.f0a3197ee7a2b15b9098e41e8913a7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e15ae91946ae27fcf92",
+    "url": "/todo-app/static/css/main.608c3ba9.chunk.css"
+  },
+  {
+    "revision": "6e15ae91946ae27fcf92",
+    "url": "/todo-app/static/js/main.6e15ae91.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/todo-app/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "acbb95a6d16e242c37f9",
+    "url": "/todo-app/static/js/2.acbb95a6.chunk.js"
+  },
+  {
+    "revision": "5c825448c34dec4a3978",
+    "url": "/todo-app/static/js/runtime~main.5c825448.js"
+  },
+  {
+    "revision": "46356c0a2935b7875039269e533c435a",
+    "url": "/todo-app/index.html"
+  }
+];
